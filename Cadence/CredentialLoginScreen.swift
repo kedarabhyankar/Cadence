87e5cecfa52f3bf10d0e8d658c586f20dd8e0ad2
Kedar Abhyankar <krabhyankar@gmail.com>
2021-05-25 23:15:21 -0700

    CredentialManager UI done

--- Cadence/CredentialLoginScreen.swift
+++ Cadence/CredentialLoginScreen.swift
@@ -12,29 +12,93 @@ import EmailValidator
 struct CredentialLoginScreen : View {
     
     @State private var emailAddress: String = ""
+    @State private var password: String = ""
     @State private var isEditing = false
     @State private var invalidEmail = false
+    @State private var invalidPassword = false
+    
     var body : some View {
-        Text("Sign in with Email")
-        VStack{
-            Text("Email Address")
+        HStack {
+            //            VStack {
+            //                Spacer()
+            //            }
+            Text("Sign in with Email").font(.title)
+            Spacer()
+        }.padding()
+        HStack {
+            Text("Email Address").bold()
             if(invalidEmail){
                 Text("Invalid Email Address!").foregroundColor(Color.red)
             }
-            TextField(
-                "Email Address",
-                text: $emailAddress
-            ) { isEditing in
+            Spacer()
+        }.padding()
+        HStack {
+            Spacer().frame(width: 30)
+            TextField("Email Address", text: $emailAddress)
+            { isEditing in
                 self.isEditing = isEditing
             } onCommit: {
-                let isEmailValid = EmailValidator.validate(email: emailAddress)
-                if(!isEmailValid){
-                    invalidEmail = true
+                if(!emailAddress.isEmpty){
+                    let isEmailValid = EmailValidator.validate(email: emailAddress)
+                    if(!isEmailValid){
+                        invalidEmail = true
+                    }
+                } else {
+                    invalidEmail = false
                 }
             }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+            .disableAutocorrection(true)
+            .keyboardType(.emailAddress)
+            Spacer()
         }
+        HStack {
+            Text("Password").bold()
+            if(invalidPassword){
+                Text("Incorrect Password!").foregroundColor(Color.red)
+            }
+            Spacer()
+        }.padding()
+        HStack {
+            Spacer().frame(width: 30)
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle()) {
+                    let didLogin, err = handleLogin(email: emailAddress, password: password)
+                    if(didLogin.0 == true){
+                        //logged in successfully
+                    } else {
+                        //didn't log in successfully, what happened?
+                        
+                    }
+                }
+            
+            Spacer()
+        }
+        Spacer().frame(height: 200)
+        //        Text("Sign in with Email").font(.title)
+        //        VStack{
+        //            HStack{
+        //                Text("Email Address").frame(alignment: .leading)
+        //                if(invalidEmail){
+        //                    Text("Invalid Email Address!").foregroundColor(Color.red)
+        //                }
+        //            }
+        //            TextField(
+        //                "Email Address",
+        //                text: $emailAddress
+        //            ) { isEditing in
+        //                self.isEditing = isEditing
+        //            } onCommit: {
+        //                let isEmailValid = EmailValidator.validate(email: emailAddress)
+        //                if(!isEmailValid){
+        //                    invalidEmail = true
+        //                }
+        //            }
+        //            Spacer().frame(minWidth: 0, idealWidth: 0, maxWidth: 0, minHeight: 100, idealHeight: 100, maxHeight: 100, alignment: .center)
     }
 }
+//}
 
 extension UIApplication {
     func addTapGestureRecognizer() {
@@ -51,4 +115,8 @@ extension UIApplication: UIGestureRecognizerDelegate {
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
         return true // set to `false` if you don't want to detect tap during other gestures
     }
+}
+
+func handleLogin(email: String, password: String) -> (Bool, String) {
+    
 }
